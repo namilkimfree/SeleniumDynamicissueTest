ac3809cbcf4652a85024643c584009fa3e2fb72b
free <free@netmarble.com>
2021-07-05 15:04:31 +0900

    Add project files.

---
+++ SeleniumDynamicissueTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace SeleniumDynamicissueTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driverOptions = new ChromeOptions();
+
+            driverOptions.AddAdditionalOption("se:recordVideo", true);
+            driverOptions.AddAdditionalOption("se:timeZone", "US/Pacific");
+            driverOptions.AddAdditionalOption("se:screenResolution", "1920x1080");
+
+            var driver = new RemoteWebDriver(new Uri("http://seleniumHub:4444"), driverOptions)
+            {
+            };
+
+        }
+    }
+}
